f989b5a91ecc6c4ccc6b73c69d97c2c9a2cc9a5c
Evgeniia Chaus <angies0606@gmail.com>
2021-01-20 17:20:21 +0300

    продолжение

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src='https://www.pinclipart.com/picdir/big/526-5262236_transparent-falling-png-phoenix-egg-png-clipart.png'/>
       </header>
+      <nav className="nav">
+        <div>
+         <a> Profile</a>
+        </div>
+        <div>
+          <a> Messages</a>
+        </div>
+        <div>
+          <a> News</a>
+        </div>
+        <div>
+          <a> Music </a>
+        </div>
+        <div>
+          <a> Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        Main content
+      </div>
     </div>
   );
 }
